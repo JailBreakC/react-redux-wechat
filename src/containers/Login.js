49711ac9d1fc0d6e89e5a78ecd1df96910fa4d2e
JailBreak <admin@vgee.cn>
2017-05-23 01:08:34 +0800

    完成登录/注册UI 状态切换

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,54 +1,74 @@
 import React, { Component } from 'react'
 import { Form, Icon, Input, Button, Checkbox } from 'antd'
+import Copyleft from '../components/Copyleft'
 import './Login.less'
 
 const FormItem = Form.Item
 
 class Login extends Component {
+  state = {
+    isSigningUp: false
+  }
+
   handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        console.log('Received values of form: ', values)
       }
-    });
+    })
+  }
+
+  toggleSignUp = (e) => {
+    console.log(this.state)
+    this.setState({
+      isSigningUp: !this.state.isSigningUp
+    })
   }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { toggleSignUp } = this
+    const { isSigningUp } = this.state
+    const { getFieldDecorator } = this.props.form
+
     return (
-      <div className="login">
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <div className="logo-ct">
-          <i className="iconfont icon-b2"></i>
+      <div className="wrap">
+        <div className="login">
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <div className="logo-ct">
+              <i className="iconfont icon-b2"></i>
+            </div>
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: '请输入用户名' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: '请输入密码' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+              )}
+            </FormItem>
+            <FormItem>
+              {
+                !isSigningUp ? getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                })(
+                  <Checkbox>保持登录</Checkbox>
+                ) : ''
+              }
+              <a className="login-form-forgot" onClick={toggleSignUp} >{ isSigningUp ?  '登录' : '马上注册！'}</a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                { isSigningUp ? '注册' : '登录'}
+              </Button>
+            </FormItem>
+          </Form>
         </div>
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: '请输入用户名' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: '请输入密码' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>保持登录</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">马上注册！</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            登录
-          </Button>
-        </FormItem>
-      </Form>
+        <Copyleft />
       </div>
     )
   }
